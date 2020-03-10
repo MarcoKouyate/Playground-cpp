a06810019212d248ee5e6905beca5597d7c7528e
Marco Kouyaté <marco.kouyate@yahoo.fr>
2020-03-10 02:43:41 +0100

    Added  instructions for github

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,12 @@
 // This is a test project in order to learn how to use Visual Studio 2019 and Git Version Control
 
+//CONSIGNES POUR NASSIM
+/*I- Récupérer ce repo 
+a) Installer Github extension pour visual studio
+b) Se connecter à Github
+c) Récupérer ce repo
+*/
+
 #include <iostream>
 
 int main() {
