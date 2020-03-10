1bb3fe44b52e4504bc5f97a830547b4edb9c982a
Nassim <latrach.nassim@gmail.com>
2020-03-10 17:02:15 +0100

    Validation des consignes

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,10 @@
 // This is a test project in order to learn how to use Visual Studio 2019 and Git Version Control
 
 //CONSIGNES POUR NASSIM
-/*I- Récupérer ce repo 
-a) Installer Github extension pour visual studio
-b) Se connecter à Github
-c) Récupérer ce repo
+/*I- Récupérer ce repo  - FAIT
+a) Installer Github extension pour visual studio - FAIT
+b) Se connecter à Github - FAIT
+c) Récupérer ce repo - FAIT
 */
 
 #include <iostream>
