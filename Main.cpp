9ab6cabc22544139b4e777c1ecd2d6290a91a0ba
Marco Kouyaté <marco.kouyate@yahoo.fr>
2020-03-10 02:41:13 +0100

    Added first instructions

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+// This is a test project in order to learn how to use Visual Studio 2019 and Git Version Control
+
+#include <iostream>
+
+int main() {
+	std::cout << "Money" << std::endl;
+	return 0;
+}
