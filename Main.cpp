9dd32005320d81c64dfb7e9b226f0ca46de6dfa2
Nassim <latrach.nassim@gmail.com>
2020-03-11 16:45:08 +0100

    Conversion de la struct pokemon en class pokemon

--- Main.cpp
+++ Main.cpp
@@ -3,37 +3,53 @@
 #include <iostream>
 #include <string>
 
+enum pokemontype { fire, water, grass };
 
-struct Pokemon {
-	std::string name;
-	int HP;
-	int attackPoints;
-	pokemonType type;
+class Pokemon {
+
+public:
+	Pokemon(std::string name, int HP, int attackPoint, pokemontype type) {
+		m_name = name;
+		m_HP = HP;
+		m_attackPoints = attackPoint;
+		m_type = type;
+	}
+
+	//Getter
+	std::string getName() {
+		return m_name;
+	}
 
 
 	//check if pokemon is dead
 	bool isDead() {
-		return (HP <= 0);
+		return (m_HP <= 0);
 	}
 
 	//to inflict damage to pokemon
 	void damage(int damage) {
-		HP = HP - damage;
-		std::cout << "Le " << name << " perd : " << damage << " PV." << std::endl;
+		m_HP = m_HP - damage;
+		std::cout << "Le " << m_name << " perd : " << damage << " PV." << std::endl;
 	}
 
 	//return informations about the pokemon
 	std::string info() {
-		return "<" +name + " [HP: " + std::to_string(HP) + "]>";
+		return "<" + m_name + " [HP: " + std::to_string(m_HP) + "]>";
 	}
 
 	//inflict damage to other pokemon
 	void attack(Pokemon& other) {
 		if (!isDead()) {
-			std::cout << name << " attaque " << other.name << std::endl;
-			other.damage(attackPoints);
+			std::cout << m_name << " attaque " << other.m_name << std::endl;
+			other.damage(m_attackPoints);
 		}
 	}
+
+private:
+	std::string m_name;
+	int m_HP;
+	int m_attackPoints;
+	pokemontype m_type;
 };
 
 void turnperturn(Pokemon& pokemon1, Pokemon& pokemon2) {
@@ -45,29 +61,27 @@ void turnperturn(Pokemon& pokemon1, Pokemon& pokemon2) {
 	}
 
 	if (pokemon1.isDead()) {
-		std::cout << pokemon1.name << " est K.O" << std::endl;
+		std::cout << pokemon1.getName() << " est K.O" << std::endl;
 	}
 	if (pokemon2.isDead()) {
-		std::cout << pokemon2.name << " est K.O" << std::endl;
+		std::cout << pokemon2.getName() << " est K.O" << std::endl;
 	}
 	std::cout << std::endl;
 }
 
-enum pokemonType {fire, water, grass};
+
 
 int main() {
 	//Pokemons structs
-	Pokemon pokemon1 = { "Amphinobi", 100, 10, water };
-	Pokemon pokemon2 = { "Brasegali", 100, 30, fire };
+	Pokemon pokemon1 ( "Amphinobi", 100, 10, water );
+	Pokemon  pokemon2 ("Brasegali", 100, 30, fire );
 
-	Pokemon pikachu = { "Pikachu", 100, 10 };
-	Pokemon link = { "Link", 85, 15 };
+	
 	
 	//First duel
 	turnperturn(pokemon1, pokemon2);
 
-	//Second duel
-	turnperturn(pikachu, link);
+	
 
 	return 0;
 }
